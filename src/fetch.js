0dc1fef8404d35b996b9c5ea2c736e972489ba36
alena <evstafienko_a@ukr.net>
2022-12-17 21:00:52 +0200

    go

--- src/fetch.js
+++ src/fetch.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-export default function cardFetchAxios(inputValue, page){
+export default async function cardFetchAxios(inputValue, page){
   const API_KAY = '31999317-3d5c3cea8ef86e3c7a76bbcc7';
   const URL = 'https://pixabay.com/api/';
-  const filter = `?key=${API_KAY}&p=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+  const filter = `?key=${API_KAY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
 
-  return axios.get(`${URL}${filter}`)
+  return await axios.get(`${URL}${filter}`)
     .then(response => {
-        // console.log(response.data);
       return response.data;
 
    })
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import cardTemplates from './templates';
-// import cardFetch from './fetch';
-import cardFetchAxios from "./fetch";
+import cardFetchAxios from './fetch';
 
 const refs = {
   formEl: document.querySelector('.search-form'),
   inputEl: document.querySelector('.search-form input'),
   btnLoadMoreEl: document.querySelector('.load-more'),
   galleryEl: document.querySelector('.gallery'),
+  infoTextEl: document.querySelector('.info-text')
 };
 
 let pageNumber = 1;
@@ -18,93 +18,107 @@ let totalHits = 0;
 let inputSpace = '';
 
 refs.formEl.addEventListener('submit', onFormElSubmit);
-refs.btnLoadMoreEl.addEventListener('click', onBtnLoadMoreElClick);
-// // window.addEventListener('scroll', onWindowScrol);
-refs.formEl.addEventListener('keydown', (e) => {
+// refs.btnLoadMoreEl.addEventListener('click', onBtnLoadMoreElClick);
+
+refs.formEl.addEventListener('keydown', e => {
   inputSpace = e.code;
-  // inputValue =  e.currentTarget.searchQuery.value;
 });
 
-function onFormElSubmit(e) {
+async function onFormElSubmit(e) {
   e.preventDefault();
-   inputValue = e.currentTarget.searchQuery.value;
+  inputValue = e.currentTarget.searchQuery.value;
   // У разі пошуку за новим ключовим словом, значення page повернути до початкового
   pageNumber = 1;
   refs.galleryEl.innerHTML = '';
- 
-  console.log(inputValue);
-  
-  if (inputValue === '' || inputSpace === "Space") {
+
+  if (inputValue === '' || inputSpace === 'Space') {
     return;
+  }
+
+  const results = await cardFetchAxios(inputValue, pageNumber);
+  totalHits = results.totalHits;
+  if (results.totalHits < 40) {
+    refs.btnLoadMoreEl.classList.add('is-hidden');
+    refs.infoTextEl.classList.remove('is-hidden');
+  } else {
+    refs.btnLoadMoreEl.classList.remove('is-hidden');
+     refs.infoTextEl.classList.add('is-hidden');
   };
 
-   cardFetchAxios(inputValue, pageNumber)
-    .then(results => {
-      cardCreate(results.hits);
-      totalHits = results.totalHits;
-    if (results.hits.length === 0) {
+  try {
+    cardCreate(results.hits);
+
+    if (totalHits === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
-    };
-    if (results.hits.length >= 1) {
+    }
+    if (totalHits >= 1) {
       Notify.success(`Hooray! We found ${totalHits} images.`);
-      refs.btnLoadMoreEl.classList.remove('is-hidden');
       loadingLazy();
-    }; })
-    .then((pageNumber += 1))
-    .catch(error => console.log(error));
+    }
+    pageNumber += 1;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function onBtnLoadMoreElClick(e) {
-  cardFetchAxios(inputValue, pageNumber)
-    .then(results => {
-      cardCreate(results.hits);  
-      if (Number(totalHits) <= 1) {
-      Notify.info(`We're sorry, but you've reached the end of search results.`);
-      refs.btnLoadMoreEl.classList.add('is-hidden');
-    };})
-    .then((pageNumber += 1))
-    .catch(error => console.log(error)); 
-};
-
-// function onWindowScrol() {
-//   const documentRect = document.documentElement.getBoundingClientRect();
-//   if ( documentRect.bottom < document.documentElement.clientHeight +150){
-//     cardFetchAxios(inputValue, pageNumber)
-//     .then(results => {
-//       cardCreate(results);  
-//       if (Number(totalHits) <= 1) {
-//       Notify.info(`We're sorry, but you've reached the end of search results.`);
-//       refs.btnLoadMoreEl.classList.add('is-hidden');
-//     };})
-//     .then((pageNumber += 1))
-//     .catch(error => console.log(error)); 
+// async function onBtnLoadMoreElClick(e) {
+//  const results = await cardFetchAxios(inputValue, pageNumber);
+// if (results.totalHits < 40) {
+//     refs.btnLoadMoreEl.classList.add('is-hidden');
+//   } else {
+//     refs.btnLoadMoreEl.classList.remove('is-hidden');
 //   };
+//   try {
+//     cardCreate(results.hits);
+//     pageNumber += 1;
+//   } catch (error) {
+//     console.log(error);
+//   }
 // }
+
+window.addEventListener('scroll', onWindowScrol);
+
+async function onWindowScrol() {
+  const documentRect = document.documentElement.getBoundingClientRect();
+  if ( documentRect.bottom < document.documentElement.clientHeight + 300){
+   
+     refs.btnLoadMoreEl.classList.add('is-hidden');
+    const results = await cardFetchAxios(inputValue, pageNumber);
+    cardCreate(results.hits);
+    pageNumber += 1;
+if (results.totalHits < 40) {
+    refs.infoTextEl.classList.remove('is-hidden');
+  } else {
+     refs.infoTextEl.classList.add('is-hidden');
+  };
+  };
+}
 
 function cardCreate(imgs) {
   const markup = imgs.map(img => cardTemplates(img)).join('');
   refs.galleryEl.insertAdjacentHTML('beforeend', markup);
-  
+
   // підключаємо бібліотеку SimpleLightbox
   const lightbox = new SimpleLightbox('.photo-card a', {
     captions: true,
     captionsDelay: 250,
   });
 
-  lightbox.refresh(); 
-};
-
-function loadingLazy(){
- const { height: cardHeight } = document
-  .querySelector(".gallery")
-  .firstElementChild.getBoundingClientRect();
-  // Метод getBoundingClientRect()повертає розмір елемента та його положення відносно вікна перегляду
-window.scrollBy({
-  top: cardHeight * -1,
-  behavior: "smooth",
-  // прокручування анімується плавно 
-}); 
+  lightbox.refresh();
 }
+
+function loadingLazy() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+  // Метод getBoundingClientRect()повертає розмір елемента та його положення відносно вікна перегляду
+  window.scrollBy({
+    top: cardHeight * -1,
+    behavior: 'smooth',
+    // прокручування анімується плавно
+  });
+}
+
